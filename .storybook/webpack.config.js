703ccdb4299600e44250ee71f99edbd51323127a
ygjack <ygjack414@hotmail.com>
2019-10-12 14:46:05 +0800

    add webpack config for storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+module.exports = async ({ config }) => {
+
+  config.resolve.alias['@'] = path.resolve(__dirname, '../src')
+  config.resolve.alias['mock'] = path.resolve(__dirname, '../mock')
+
+  return config
+}
