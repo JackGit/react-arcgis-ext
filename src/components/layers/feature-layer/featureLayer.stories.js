19f19a4c5ed81ce4d9f9ca56b37bacf08e2d401e
ygjack <ygjack414@hotmail.com>
2019-10-25 17:03:36 +0800

    add more stories

--- src/components/layers/feature-layer/featureLayer.stories.js
+++ src/components/layers/feature-layer/featureLayer.stories.js
@@ -2,12 +2,12 @@ import React from 'react'
 import Map from '@/components/map/Map'
 import Graphic from '@/components/graphic/Graphic'
 import Search from '@/components/widgets/search/Search'
+import Bookmarks from '@/components/widgets/bookmarks/Bookmarks'
 import FeatureLayer from './FeatureLayer'
 import DataLoader from '.storybook/components/DataLoader'
 import { fetchImageAsFeatures } from 'mock/features'
 
 import { WebMap, WebScene, Scene  } from '@esri/react-arcgis'
-import { transparentize } from '_polished@3.4.1@polished'
 
 
 export default { title: 'FeatureLayer' }
@@ -153,6 +153,94 @@ export const highlightPointFeatures = () => {
   )
 }
 
+export const labelFeaturesUsingArcadeExpressions = () => {
+  const windArcade = `
+    var DEG = $feature.WIND_DIRECT;
+    var SPEED = $feature.WIND_SPEED;
+    var DIR = When( SPEED == 0, "",
+      (DEG < 22.5 && DEG >= 0) || DEG > 337.5, "N",
+      DEG >= 22.5 && DEG < 67.5, "NE",
+      DEG >= 67.5 && DEG < 112.5, "E",
+      DEG >= 112.5 && DEG < 157.5, "SE",
+      DEG >= 157.5 && DEG < 202.5, "S",
+      DEG >= 202.5 && DEG < 247.5, "SW",
+      DEG >= 247.5 && DEG < 292.5, "W",
+      DEG >= 292.5 && DEG < 337.5, "NW", "" );
+    return SPEED + " mph " + DIR;
+  `
+
+  const featureLayerProperties = {
+    url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/weather_stations_010417/FeatureServer/0",
+    elevationInfo: {
+      mode: "relative-to-ground",
+      offset: 1000
+    },
+    renderer: {
+      type: "simple",
+      symbol: {
+        type: "point-3d",
+        symbolLayers: [{
+          type: "object",
+          material: { color: "#a38a8a" },
+          resource: { primitive: "tetrahedron" },
+          width: 10000,
+          depth: 20000,
+          height: 1000
+        }]
+      },
+      visualVariables: [{
+        type: "rotation",
+        valueExpression: "$feature.WIND_DIRECT - 180",
+        axis: "heading"
+      }]
+    },
+    labelingInfo: [
+      { min: 0, max: 67.5, color: "#4c82c4" },
+      { min: 67.5, max: 157.5, color: "#6c4cc4" },
+      { min: 157.5, max: 247.5, color: "#ae4cc4" },
+      { min: 247.5, max: 337.5, color: "#c44c88" },
+      { min: 337.5, max: 360, color: "#4c82c4" }
+    ].map(windClass => ({
+      labelExpressionInfo: { expression: windArcade },
+      where: "WIND_DIRECT > " + windClass.min + " AND WIND_DIRECT <= " + windClass.max,
+      labelPlacement: "above-center",
+      symbol: {
+        type: "label-3d",
+        symbolLayers: [{
+          type: "text",
+          material: { color: "white" },
+          halo: { color: windClass.color, size: 1 },
+          size: 12
+        }]
+      }
+    })),
+    opacity: 1
+  }
+
+  return (
+    <div style={{width:'100vw',height:'100vh'}}>
+      <Scene
+        mapProperties={{ basemap: 'topo' }}
+        viewProperties={{
+          camera: {
+            position: {
+              x: -10930027,
+              y: 5458284,
+              z: 126663,
+              spatialReference: { wkid: 102100 }
+            },
+            heading: 63,
+            tilt: 73
+          },
+          constraints: { altitude: { min: 100000 } }
+        }}
+      >
+        <FeatureLayer properties={featureLayerProperties} />
+      </Scene>
+    </div>
+  )
+}
+
 export const addLabelsToAFeatureLayer = () => {
   const featureLayerProperties = {
     portalItem: {
@@ -201,6 +289,155 @@ export const addLabelsToAFeatureLayer = () => {
       >
         <FeatureLayer properties={featureLayerProperties} />
         <Search position="top-right" />
+      </WebMap>
+    </div>
+  )
+}
+
+export const multilineLabels = () => {
+  const expression = `
+      var DEG = $feature.WIND_DIRECT;
+      var SPEED = $feature.WIND_SPEED;
+      var DIR = When( SPEED == 0, null,
+        (DEG < 22.5 && DEG >= 0) || DEG > 337.5, 'N',
+        DEG >= 22.5 && DEG < 67.5, 'NE',
+        DEG >= 67.5 && DEG < 112.5, 'E',
+        DEG >= 112.5 && DEG < 157.5, 'SE',
+        DEG >= 157.5 && DEG < 202.5, 'S',
+        DEG >= 202.5 && DEG < 247.5, 'SW',
+        DEG >= 247.5 && DEG < 292.5, 'W',
+        DEG >= 292.5 && DEG < 337.5, 'NW', null );
+      var WIND = SPEED + ' mph ' + DIR;
+      var TEMP = Round($feature.TEMP) + '° F';
+      var RH = $feature.R_HUMIDITY + '% RH';
+      var NAME = $feature.STATION_NAME;
+      var labels = [ NAME, TEMP, WIND, RH ];
+      return Concatenate(labels, TextFormatting.NewLine);
+    `
+  const featureLayerProperties = {
+    url: 'https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/weather_stations_010417/FeatureServer/0',
+    renderer: {
+      type: "simple",
+      symbol: {
+        type: "simple-marker",
+        color: [75, 75, 75, 0.7],
+        size: 4,
+        outline: null
+      }
+    },
+    labelingInfo: [{
+      labelExpressionInfo: { expression },
+      labelPlacement: "center-right",
+      minScale: 250000000,
+      symbol: {
+        type: "text", // autocasts as new TextSymbol()
+        font: {
+          size: 9,
+          family: "Noto Sans"
+        },
+        horizontalAlignment: "left",
+        color: "#2b2b2b"
+      }
+    }]
+  }
+  return (
+    <div style={{width:'100vw',height:'100vh'}}>
+      <WebMap
+        id="372b7caa8fe340b0a6300df93ef18a7e"
+        viewProperties={{
+          center: [-117.842, 33.799],
+          zoom: 10
+        }}
+      >
+        <FeatureLayer properties={featureLayerProperties} />
+      </WebMap>
+    </div>
+  )
+}
+
+export const addMultipleLabelClassesToALayer = () => {
+  const minScale = 2500000
+  const createTextSymbol = color => ({
+    type: "text",
+    font: { size: 12, weight: "bold" },
+    color: "white",
+    haloColor: color,
+    haloSize: 1
+  })
+
+  const nameClass = {
+    labelPlacement: "below-right",
+    labelExpressionInfo: { expression: '$feature.STATION_NAME' },
+    minScale: minScale
+  }
+  nameClass.symbol = createTextSymbol("black")
+
+  const humidityClass = {
+    labelExpressionInfo: { expression: `$feature.R_HUMIDITY + '% RH'` },
+    labelPlacement: "below-left",
+    minScale: minScale
+  }
+  humidityClass.symbol = createTextSymbol("#c17c47")
+
+  const windClass = {
+    labelExpressionInfo: {
+      expression: `var DEG = $feature.WIND_DIRECT;
+      var SPEED = $feature.WIND_SPEED;
+      var DIR = When( SPEED == 0, "",
+        (DEG < 22.5 && DEG >= 0) || DEG > 337.5, "N",
+        DEG >= 22.5 && DEG < 67.5, "NE",
+        DEG >= 67.5 && DEG < 112.5, "E",
+        DEG >= 112.5 && DEG < 157.5, "SE",
+        DEG >= 157.5 && DEG < 202.5, "S",
+        DEG >= 202.5 && DEG < 247.5, "SW",
+        DEG >= 247.5 && DEG < 292.5, "W",
+        DEG >= 292.5 && DEG < 337.5, "NW", "" );
+      return SPEED + " mph " + DIR;`
+    },
+    labelPlacement: "above-right",
+    minScale: minScale
+  }
+  windClass.symbol = createTextSymbol("#3ba53f")
+
+  const tempArcade = `Round($feature.TEMP) + '° F';`
+  const highTempClass = {
+    labelExpressionInfo: { expression: tempArcade },
+    labelPlacement: "above-left",
+    where: "TEMP > 32"
+  }
+  highTempClass.symbol = createTextSymbol("#f47742")
+
+  const lowTempClass = {
+    labelExpressionInfo: { expression: tempArcade },
+    labelPlacement: "above-left",
+    where: "TEMP <= 32"
+  }
+  lowTempClass.symbol = createTextSymbol("#4792c1")
+
+  const featureLayerProperties = {
+    url: 'https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/weather_stations_010417/FeatureServer/0',
+    renderer: {
+      type: "simple",
+      symbol: {
+        type: "simple-marker",
+        color: [255, 255, 255, 0.6],
+        size: 4,
+        outline: { color: [0, 0, 0, 0.4], width: 0.5 }
+      }
+    },
+    labelingInfo: [nameClass, humidityClass, lowTempClass, highTempClass, windClass]
+  }
+  return (
+    <div style={{width:'100vw',height:'100vh'}}>
+      <WebMap
+        id="f0e331d7986041ba8267298f579b3afe"
+        viewProperties={{
+          center: [-117.842, 33.799],
+          zoom: 10
+        }}
+      >
+        <FeatureLayer properties={featureLayerProperties} />
+        <Bookmarks position="top-right" />
       </WebMap>
     </div>
   )
