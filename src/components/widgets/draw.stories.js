c3ec009944623d0a45f746155a4e58f780556a60
ygjack <ygjack414@hotmail.com>
2019-11-08 13:26:46 +0800

    add sketch storyb

---
+++ src/components/widgets/draw.stories.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { WebMap, WebScene, Scene, Map  } from '@esri/react-arcgis'
+import Sketch from './sketch/Sketch'
+import GraphicsLayer from '@/components/layers/graphics-layer/GraphicsLayer'
+
+export default { title: 'Draw' }
+
+class SketchStory extends Component {
+  state = {
+    graphicsLayer: null
+  }
+  
+  render () {
+    return (
+      <div style={{width:'100vw',height:'100vh'}}>
+        <Map
+          mapProperties={{ basemap: 'streets' }}
+          viewProperties={{
+            zoom: 5,
+            center: [90, 45]
+          }}
+        >
+          <GraphicsLayer onLoad={graphicsLayer => this.setState({ graphicsLayer })} />
+          {this.state.graphicsLayer ? <Sketch position="top-right" properties={{ layer: this.state.graphicsLayer }} /> : <div></div>}
+        </Map>
+      </div>
+    )
+  }
+}
+export const sketch = () => <SketchStory />
